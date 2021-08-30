96b18c74c6f2aa93d44e9807227b89a1ce5eee73
joaovictor72 <joaovictor72@hotmail.com>
2021-08-30 20:50:28 -0300

    Leitor de QR code

---
+++ Leitor.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+with open ('myDataFile.text') as f:
+    myDataList = f.read().splitlines()
+
+
+
+while True:
+    sucess, img = cap.read()
+    for barcode in decode(img):
+        myData = barcode.data.decode('utf-8')
+        print(myData)
+        if myData in myDataList:
+            myOutput = 'Authorized'
+            myColor = (0,255,0)
+        else:
+            myOutput = 'Un-Authorized'
+            myColor = (0,0,255)
+
+        pts = np.array([barcode.polygon],np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img,[pts],True,myColor,5)
+        pts2 = barcode.rect
+        cv2.putText(img,myOutput,(pts2[0], pts2[1]), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,myColor,2)
+    cv2.imshow('Result',img)
+    cv2.waitKey(1)
